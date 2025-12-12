4d33960417cc47b7a757a70c1fd427129c6d5580
Will Killian <william.killian@outlook.com>
2025-12-11 23:08:27 -0500

    Day 11: Prefer explicit memory spaces
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- day11.cpp
+++ day11.cpp
@@ -9,8 +9,11 @@
 #include <cuda/std/atomic>
 #include <cuda/std/span>
 
+#include <nvexec/nvtx.cuh>
 #include <nvexec/stream_context.cuh>
 #include <stdexec/execution.hpp>
+
+#include <thrust/device_vector.h>
 
 #include <nvtx3/nvtx3.hpp>
 
@@ -18,12 +21,9 @@ namespace ex = stdexec;
 
 // Graph in CSR format
 struct Graph {
-  std::vector<int> row_offsets;     // size = num_nodes + 1
-  std::vector<int> col_indices;     // size = num_edges
-  std::vector<int> out_degree;      // size = num_nodes
-  std::vector<int> in_degree;       // size = num_nodes
-  std::vector<int> row_offsets_rev; // size = num_nodes + 1
-  std::vector<int> col_indices_rev; // size = num_edges
+  thrust::device_vector<int> out_degree;      // size = num_nodes
+  thrust::device_vector<int> row_offsets_rev; // size = num_nodes + 1
+  thrust::device_vector<int> col_indices_rev; // size = num_edges
   int num_nodes{0};
   std::unordered_map<std::string, int> node_ids;
 };
@@ -117,79 +117,88 @@ auto parse() -> Graph {
     }
   }
   return Graph{
-      .row_offsets = std::move(h_row_offsets),
-      .col_indices = std::move(h_col_indices),
-      .out_degree = std::move(h_out_degree),
-      .in_degree = std::move(h_in_degree),
-      .row_offsets_rev = std::move(h_row_offsets_rev),
-      .col_indices_rev = std::move(h_col_indices_rev),
+      .out_degree = thrust::device_vector<int>(h_out_degree),
+      .row_offsets_rev = thrust::device_vector<int>(h_row_offsets_rev),
+      .col_indices_rev = thrust::device_vector<int>(h_col_indices_rev),
       .num_nodes = static_cast<int>(num_nodes),
       .node_ids = std::move(node_ids),
   };
 }
 
-auto count(Graph const &graph, int source, int dest, auto scheduler, auto policy) -> int64_t {
+auto count(Graph const &graph, int source, int dest, auto scheduler,
+           auto policy) -> int64_t {
+  nvtx3::scoped_range _{"Count"};
   std::size_t const num_nodes(graph.num_nodes);
 
-  auto paths{std::make_unique<int64_t[]>(num_nodes)};
-  auto out_degree{std::make_unique<int[]>(num_nodes)};
-  auto frontier{std::make_unique<int[]>(num_nodes)};
-  auto next_frontier{std::make_unique<int[]>(num_nodes)};
-  auto frontier_size{std::make_unique<int>(0)};
-  auto next_frontier_size{std::make_unique<int>(0)};
+  thrust::device_vector<int64_t> paths(num_nodes);
+  thrust::device_vector<int> out_degree(graph.out_degree.size());
+  thrust::device_vector<int> frontier(num_nodes);
+  thrust::device_vector<int> next_frontier(num_nodes);
+  thrust::device_vector<int> frontier_size(2);
+  thrust::copy(graph.out_degree.begin(), graph.out_degree.end(), out_degree.begin());
 
-  cuda::std::span paths_span{paths.get(), num_nodes};
-  cuda::std::span out_degree_span{out_degree.get(), num_nodes};
-  cuda::std::span frontier_span{frontier.get(), num_nodes};
-  cuda::std::span next_frontier_span{next_frontier.get(), num_nodes};
+  cuda::std::span row_offsets_rev_span{thrust::raw_pointer_cast(graph.row_offsets_rev.data()), graph.row_offsets_rev.size()};
+  cuda::std::span col_indices_rev_span{thrust::raw_pointer_cast(graph.col_indices_rev.data()), graph.col_indices_rev.size()};
 
-  cuda::std::span out_degrees{graph.out_degree};
-  cuda::std::span row_offsets_rev_span{graph.row_offsets_rev};
-  cuda::std::span col_indices_rev_span{graph.col_indices_rev};
+  cuda::std::span paths_span{thrust::raw_pointer_cast(paths.data()), paths.size()};
+  cuda::std::span out_degree_span{thrust::raw_pointer_cast(out_degree.data()), out_degree.size()};
+  cuda::std::span frontier_span{thrust::raw_pointer_cast(frontier.data()), frontier.size()};
+  cuda::std::span next_frontier_span{thrust::raw_pointer_cast(next_frontier.data()), next_frontier.size()};
 
-  auto fs_ptr{frontier_size.get()}, nfs_ptr{next_frontier_size.get()};
+  cuda::std::span sizes_span{thrust::raw_pointer_cast(frontier_size.data()), frontier_size.size()};
 
-  auto init = ex::schedule(scheduler) | ex::bulk(policy, num_nodes, [=](int u) {
-                cuda::std::atomic_ref<int> frontier_size{*fs_ptr};
-                paths_span[u] = (u == dest) ? 1 : 0;
-                out_degree_span[u] = (u == dest) ? 0 : out_degrees[u];
-                if (out_degree_span[u] == 0) {
-                  frontier_span[frontier_size.fetch_add(1)] = u;
-                }
-              });
-  ex::sync_wait(std::move(init));
-
+  int size{0};
   bool curr_active{true};
-  int* size_ptr = curr_active ? fs_ptr : nfs_ptr;
-  while (true) {
-    if (int const size{cuda::std::atomic_ref{*size_ptr}.load()}; size > 0) {
-      auto process =
+  auto init =
+      ex::transfer_just(scheduler, curr_active) |
+      nvexec::nvtx::push("Init") |
+      ex::bulk(policy, num_nodes,
+               [=](int u, bool curr_active) {
+                 cuda::std::atomic_ref frontier_size{sizes_span[curr_active]};
+                 paths_span[u] = (u == dest) ? 1 : 0;
+                 auto const new_val{(u == dest) ? 0 : out_degree_span[u]};
+                 out_degree_span[u] = new_val;
+                 if (new_val == 0) {
+                   frontier_span[frontier_size.fetch_add(1)] = u;
+                 }
+               }) |
+      ex::then([=](bool curr_active) {
+        cuda::std::atomic_ref frontier_size{sizes_span[curr_active]};
+        return frontier_size.load();
+      }) |
+      nvexec::nvtx::pop();
+  std::tie(size) = ex::sync_wait(std::move(init)).value();
+  for (;size > 0; curr_active = not curr_active) {
+    auto process =
         ex::transfer_just(scheduler, curr_active) |
-        ex::bulk(policy, size, [=](int j, bool curr_active) {
-          auto active_span = curr_active ? frontier_span : next_frontier_span;
-          auto next_span = curr_active ? next_frontier_span : frontier_span;
-          int *next_size_ptr = curr_active ? nfs_ptr : fs_ptr;
-          cuda::std::atomic_ref<int> next_frontier_size{*next_size_ptr};
-          int const v{active_span[j]};
-          int const start{row_offsets_rev_span[v]}, end{row_offsets_rev_span[v + 1]};
-          auto const span{col_indices_rev_span.subspan(start, end - start)};
-          for (int const u : span) {
-            cuda::std::atomic_ref uref{paths_span[u]}, vref{paths_span[v]};
-            uref += vref;
-            cuda::std::atomic_ref od_ref{out_degree_span[u]};
-            if (--od_ref == 0) {
-              int const idx{next_frontier_size++};
-              next_span[idx] = u;
-            }
-          }
-        });
-      ex::sync_wait(std::move(process));
-      curr_active = not curr_active;
-      cuda::std::atomic_ref{*size_ptr}.store(0);
-      size_ptr = curr_active ? fs_ptr : nfs_ptr;
-    } else {
-      break;
-    }
+        nvexec::nvtx::push("Process") |
+        ex::bulk(
+            policy, size,
+            [=](int j, bool curr_active) {
+              cuda::std::span active_span{curr_active ? frontier_span : next_frontier_span};
+              cuda::std::span next_span{curr_active ? next_frontier_span : frontier_span};
+              cuda::std::atomic_ref next_frontier_size{sizes_span[not curr_active]};
+              int const v{active_span[j]};
+              int const start{row_offsets_rev_span[v]}, end{row_offsets_rev_span[v + 1]};
+              auto const span{col_indices_rev_span.subspan(start, end - start)};
+              for (int const u : span) {
+                cuda::std::atomic_ref uref{paths_span[u]}, vref{paths_span[v]};
+                uref += vref;
+                cuda::std::atomic_ref od_ref{out_degree_span[u]};
+                if (--od_ref == 0) {
+                  int const idx{next_frontier_size++};
+                  next_span[idx] = u;
+                }
+              }
+            }) |
+        ex::then([=](bool curr_active) {
+          cuda::std::atomic_ref curr_size{sizes_span[curr_active]};
+          cuda::std::atomic_ref next_size{sizes_span[not curr_active]};
+          curr_size.store(0);
+          return next_size.exchange(0);
+        }) |
+        nvexec::nvtx::pop();
+    std::tie(size) = ex::sync_wait(std::move(process)).value();
   }
   return paths[source];
 }
@@ -207,16 +216,21 @@ auto part2(Graph const &graph, auto scheduler, auto policy) -> int64_t {
   int const dac{graph.node_ids.at("dac")};
   int const fft{graph.node_ids.at("fft")};
   int const out{graph.node_ids.at("out")};
-  return (count(graph, svr, dac, scheduler, policy) *  //
-          count(graph, dac, fft, scheduler, policy) *  //
-          count(graph, fft, out, scheduler, policy)) + //
-         (count(graph, svr, fft, scheduler, policy) *  //
-          count(graph, fft, dac, scheduler, policy) *  //
-          count(graph, dac, out, scheduler, policy));
+  auto check = [&](int source, int dest) -> int64_t {
+    return count(graph, source, dest, scheduler, policy);
+  };
+  int64_t const mid1{check(dac, fft)}, mid2{check(fft, dac)};
+  int64_t total{0};
+  if (mid1 != 0) {
+    total = check(svr, dac) * mid1 * check(fft, out);
+  }
+  if (mid2 != 0) {
+    total += check(svr, fft) * mid2 * check(dac, out);
+  }
+  return total;
 }
 
 auto main() -> int {
-  cudaSetDevice(0);
   nvtx3::scoped_range _{"Day 11"};
   auto const graph{parse()};
   nvexec::stream_context stream_ctx{};
